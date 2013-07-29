b0ddf8a6d29b63ae94887df3aa37cd7c1bd95113
Patrickmnts <patrickmnts@gmail.com>
2013-07-29 13:59:03 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Flowfree::Application.config.secret_key_base = '61b4ff074a7b079a7afe28f2745dd70fc2d990e2b41d629864e67024ac1cdd43e2c6c08775db55ba7630a69bac60851cd286f6730682c3ace4a34622416efccf'
