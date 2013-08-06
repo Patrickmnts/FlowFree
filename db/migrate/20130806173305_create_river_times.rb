5c97766fb6dffd7889323a2ba7a4cfec43f9ddbb
Daniel Stark <daniel@studiostark.com>
2013-08-06 10:33:23 -0700

    add rivertime model

---
+++ db/migrate/20130806173305_create_river_times.rb
@@ -0,0 +1,11 @@
+class CreateRiverTimes < ActiveRecord::Migration
+  def change
+    create_table :river_times do |t|
+      t.string :site_code
+      t.datetime :time
+      t.integer :cfs_value
+
+      t.timestamps
+    end
+  end
+end
