4b88105a5a190dc610cd69c24e0880a88b4d199f
Daniel Stark <daniel@studiostark.com>
2013-08-06 10:18:44 -0700

    remove cfs_value and refresh_time from river model

--- db/migrate/20130731155526_create_rivers.rb
+++ db/migrate/20130731155526_create_rivers.rb
@@ -3,11 +3,9 @@ class CreateRivers < ActiveRecord::Migration
     create_table :rivers do |t|
       t.string :site_name
       t.string :site_code
-      t.datetime :refresh_time
       t.string :latitude
       t.string :longitude
       t.string :county_id
-      t.string :cfs_value
       t.string :state
 
       t.timestamps
