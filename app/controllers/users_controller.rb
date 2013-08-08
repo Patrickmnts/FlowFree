b848121a3086355b470824922abe350bb0dc40e5
Patrickmnts <patrickmnts@gmail.com>
2013-08-07 20:24:43 -0700

    begin creating user controller & model (using Sample app as template)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,56 @@
 class UsersController < ApplicationController
   before_filter :authenticate_user!
+  before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user, only: :destroy
 
   def new
     @user = User.new
   end
-end
+
+  def edit
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @rivers = @user.rivers.paginate(page: params[:page])
+  end
+
+  def create
+    # user_params is a call to the allowed params function at bottom.
+    @user = User.new(user_params)
+    if @user.save
+      sign_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_url
+  end
+
+
+  private
+
+    def user_params
+      params.require(:user).permit(:email, :password, :password_confirmation)
+    end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ActiveRecord::Base
-
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :token_authenticatable, :timeoutable
 
+  has_many :rivers
+  before_save { |user| user.email = email.downcase }
 
 end
