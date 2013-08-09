7137725a6659add17885217755b3c17aa1df1d8b
ThirdAndjel <thirdandjel@gmail.com>
2013-08-09 01:22:46 -0700

    add tests for home page elements

--- spec/controllers/site_controller_spec.rb
+++ spec/controllers/site_controller_spec.rb
@@ -1,17 +1,7 @@
-  require 'spec_helper'
+require 'spec_helper'
 
 describe "Site pages" do
   describe "Home page" do
-    # this might be more appropriate under spec/site/index
-    it "should have the title FlowFree" do
-      visit '/'
-      expect(page).to have_title('FlowFree')
-    end
-    it "should have a h2 header 'Search for...'" do
-      visit '/'
-      expect(page).to have_selector('h2', :text => "Search for River Flows By State")
-    end
-    # ///////////////////////////////////////////////////////////
     it "should go to sign-up page" do
       visit '/'
       click_link 'Register'
