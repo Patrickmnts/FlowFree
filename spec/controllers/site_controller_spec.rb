94333f2b79e1ffc7c6c343c8cf34ca824a5fbb01
ThirdAndjel <thirdandjel@gmail.com>
2013-08-09 00:12:37 -0700

    update tests to match changes

--- spec/controllers/site_controller_spec.rb
+++ spec/controllers/site_controller_spec.rb
@@ -7,20 +7,38 @@ describe "Site pages" do
       visit '/'
       expect(page).to have_title('FlowFree')
     end
-    it "should have a h1 header 'Search for...'" do
+    it "should have a h2 header 'Search for...'" do
       visit '/'
-      expect(page).to have_selector('h1', :text => "Search for River Flows By State")
+      expect(page).to have_selector('h2', :text => "Search for River Flows By State")
     end
     # ///////////////////////////////////////////////////////////
     it "should go to sign-up page" do
       visit '/'
-      click_link 'signup link....'
-      expect(page).to have_content('Sign up')
+      click_link 'Register'
+      expect(page).to have_button('Sign up')
     end
-    # it "should go to rivers page" do
-    #   visit '/'
-    #   click_button 'Go!'
-    #   current_url.include?('rivers/index') == true
-    # end
+    it "should go to sign-in page" do
+      visit '/'
+      click_link 'Sign in'
+      expect(page).to have_button('Sign in')
+    end
+    it "should go to rivers page" do
+      visit '/'
+      click_button 'Go!'
+      puts current_url
+      current_path.should == '/rivers/by_state/'
+    end
+    it "should go to rivers page of Arizona" do
+      visit '/'
+      select("Arizona", :from => "state")
+      click_button 'Go!'
+      current_path.should == '/rivers/by_state/az'
+    end
+    it "should go to rivers page of Oregon" do
+      visit '/'
+      select("Oregon", :from => "state")
+      click_button 'Go!'
+      current_path.should == '/rivers/by_state/or'
+    end
   end
 end
