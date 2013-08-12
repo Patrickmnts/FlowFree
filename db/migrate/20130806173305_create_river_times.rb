4afc7c9ec1ef91c31cbb4749f941ee1e8a005fd5
Daniel Stark <daniel@studiostark.com>
2013-08-12 13:07:58 -0700

    rework relations so we can actually link via site_code

--- db/migrate/20130806173305_create_river_times.rb
+++ db/migrate/20130806173305_create_river_times.rb
@@ -1,7 +1,7 @@
 class CreateRiverTimes < ActiveRecord::Migration
   def change
     create_table :river_times  do |t|
-      t.string :site_code
+      t.integer :site_code
       t.datetime :time
       t.integer :cfs_value
 
