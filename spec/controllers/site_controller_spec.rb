bccd306d1eb47295470af3cc0cc089be748c7c18
ThirdAndjel <thirdandjel@gmail.com>
2013-08-13 14:11:02 -0700

    refactor the existing tests a bit

--- spec/controllers/site_controller_spec.rb
+++ spec/controllers/site_controller_spec.rb
@@ -2,75 +2,70 @@ require 'spec_helper'
 
 describe "Site pages" do
   describe "Home page" do
-    it "should go to sign-up page" do
+    before :each do
       visit '/'
+    end
+    it "should go to sign-up page" do
       within(:css, '.button-group.right') do
         click_link 'Register'
       end
       expect(page).to have_content("devise/registrations:new")
     end
     it "should go to sign-up page" do
-      visit '/'
       within(:css, '.button-group.right') do
         click_link 'Register'
       end
       current_path.should == '/users/sign_up'
     end
     it "should go to sign-up page" do
-      visit '/'
       click_link 'Register Now!'
       current_path.should == '/users/sign_up'
     end
     it "should go to sign-in page" do
-      visit '/'
       within(:css, '.button-group.right') do
         click_link 'Sign In'
       end
       expect(page).to have_content("devise/sessions:new")
     end
     it "should go to sign-in page" do
-      visit '/'
       within(:css, '.button-group.right') do
         click_link 'Sign In'
       end
       current_path.should == '/users/sign_in'
     end
     it "should go to rivers page of Alaska" do
-      visit '/'
       select('Alaska', :from => 'state')
       click_button 'Go!'
       current_path.should == '/rivers/by_state/ak'
     end
     it "should go to rivers page of Oregon" do
-      visit '/'
       select('Oregon', :from => 'state')
       click_button 'Go!'
       current_path.should == '/rivers/by_state/or'
     end
     it "should go to rivers page of chosen river" do
-      visit '/'
       fill_in 'search', :with => "Willamette"
       click_button 'Search'
       current_path.should == '/rivers/by_name/Willamette'
     end
     it "should go to rivers page of chosen river" do
-      visit '/'
       fill_in 'search', :with => "Mississippi"
       click_button 'Search'
       current_path.should == '/rivers/by_name/Mississippi'
     end
   end
   describe "Register page" do
-    it "should go to the welcome page after creating a new user" do
+    before :each do
       visit '/users/sign_up'
+    end
+    it "should go to the welcome/home page after creating a new user" do
       fill_in 'user_email', :with => 'johns@example.com'
       fill_in 'user_password', :with => 'masterparty'
       fill_in 'user_password_confirmation', :with => 'masterparty'
       click_button 'Sign Up'
       expect(page).to have_content("Welcome!")
     end
-    it "should go to the welcome page after creating a new user" do
-      visit '/users/sign_up'
+    it "should go to the welcome/home page after creating a new user" do
       fill_in 'user_email', :with => 'johnp@example.com'
       fill_in 'user_password', :with => 'masterparty'
       fill_in 'user_password_confirmation', :with => 'masterparty'
@@ -78,34 +73,28 @@ describe "Site pages" do
       expect(page).to have_content("site:index")
     end
     it "should go back to the home page" do
-      visit '/users/sign_up'
       click_link 'FlowFree'
       current_path.should == '/'
     end
     it "should go back to the home page" do
-      visit '/users/sign_up'
       click_link 'FlowFree'
       expect(page).to have_content("site:index")
     end
     it "should go to the forgot your password page" do
-      visit '/users/sign_up'
       click_link "Forgot your password?"
       current_path.should == '/users/password/new'
     end
     it "should go to the forgot your password page" do
-      visit '/users/sign_up'
       click_link "Forgot your password?"
       expect(page).to have_content("devise/passwords:new")
     end
     it "should go to the sign-in page" do
-      visit '/users/sign_up'
       within(:css, '.button-group.right') do
         click_link 'Sign In'
       end
       current_path.should == '/users/sign_in'
     end
     it "should go to the sign-in page" do
-      visit '/users/sign_up'
       within(:css, '.button-group.right') do
         click_link 'Sign In'
       end
--- spec/views/site/index.html.erb_spec.rb
+++ spec/views/site/index.html.erb_spec.rb
@@ -1,40 +1,37 @@
 require 'spec_helper'
 
-describe "Home page" do
-  it "should have the title FlowFree" do
-    visit '/'
-    expect(page).to have_title('FlowFree')
-  end
-  it "should have content 'Search for...'" do
-    visit '/'
-    expect(page).to have_content("Search current flows")
-  end
-  it "should have a register button in the top right corner" do
-    visit '/'
-    within(:css, '.button-group.right') do
-      expect(page).to have_link('Register')
+describe "Site pages" do
+  describe "Home page" do
+    before :each do
+      visit '/'
     end
-  end
-  it "should have a sign-in button in the top right corner" do
-    visit '/'
-    within(:css, '.button-group.right') do
-      expect(page).to have_link('Sign In')
+    it "should have the title FlowFree" do
+      expect(page).to have_title('FlowFree')
     end
-  end
-  it "should have a drop-down menu of states" do
-    visit '/'
-    expect(page).to have_field('state')
-  end
-  it "should have a state submit button" do
-    visit '/'
-    expect(page).to have_button('Go!')
-  end
-  it "should have a search bar" do
-    visit '/'
-    expect(page).to have_field('search')
-  end
-  it "should have a search submit button" do
-    visit '/'
-    expect(page).to have_button('Search')
+    it "should have content 'Search for...'" do
+      expect(page).to have_content("Search current flows")
+    end
+    it "should have a register button in the top right corner" do
+      within(:css, '.button-group.right') do
+        expect(page).to have_link('Register')
+      end
+    end
+    it "should have a sign-in button in the top right corner" do
+      within(:css, '.button-group.right') do
+        expect(page).to have_link('Sign In')
+      end
+    end
+    it "should have a drop-down menu of states" do
+      expect(page).to have_field('state')
+    end
+    it "should have a state submit button" do
+      expect(page).to have_button('Go!')
+    end
+    it "should have a search bar" do
+      expect(page).to have_field('search')
+    end
+    it "should have a search submit button" do
+      expect(page).to have_button('Search')
+    end
   end
 end
